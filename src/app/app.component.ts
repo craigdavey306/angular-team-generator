b220f08f30e2dcdd903f3f2f7ed7627ae25b7239
Craig Davey <cdavey306@gmail.com>
2023-11-28 17:34:59 -0500

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,71 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TeamComponent } from './team/team.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [CommonModule, RouterOutlet, TeamComponent],
 })
 export class AppComponent {
-  title = 'team-app';
+  newMemberName = '';
+  members: string[] = [];
+  errorMessage = '';
+  numberOfTeams: number | '' = '';
+  teams: string[][] = [];
+
+  onNumberofTeamsInput(value: string) {
+    this.numberOfTeams = +value;
+  }
+
+  generateTeams() {
+    if (!this.numberOfTeams || this.numberOfTeams < 0) {
+      this.errorMessage = 'Invalid number of teams.';
+      return;
+    }
+
+    if (this.members.length < this.numberOfTeams) {
+      this.errorMessage = 'There are not enough members to generate teams.';
+      return;
+    }
+
+    this.errorMessage = '';
+    const allMembers = [...this.members];
+
+    while (allMembers.length) {
+      for (let i = 0; i < this.numberOfTeams; i++) {
+        const randomIndex = Math.floor(Math.random() * allMembers.length);
+        const member = allMembers.splice(randomIndex, 1)[0];
+
+        if (!member) break;
+
+        if (this.teams[i]) {
+          this.teams[i].push(member);
+        } else {
+          this.teams[i] = [member];
+        }
+      }
+    }
+
+    this.members = [];
+    this.numberOfTeams = '';
+  }
+
+  addMember() {
+    if (!this.newMemberName) {
+      this.errorMessage = 'Name must have a value.';
+      return;
+    }
+
+    this.errorMessage = '';
+    this.members.push(this.newMemberName);
+    this.newMemberName = '';
+  }
+
+  onInput(member: string) {
+    this.newMemberName = member;
+  }
 }
